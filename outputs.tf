ff09e873e50981e03e168f07c1510ed34b0859c9
Raghu <106529589+RAGHUKHAJURIA@users.noreply.github.com>
2026-04-02 02:29:51 +0530

    [NeuroOps] IaC Setup: Auto-provisioning foundation infrastructure natively (Terraform)

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,20 @@
 output "container_url" {
-  description = "URL to access the deployed Docker container."
-  value       = length(docker_container.app_container) > 0 ? "http://localhost:${docker_container.app_container[0].ports[0].external}" : "Container not deployed (mode not 'container_only' or 'full')."
-  sensitive   = false
+  description = "The URL to access the running Docker container, if deployed."
+  value = var.mode == "container_only" || var.mode == "full" ? (
+    # Check if the container resource was actually created (count > 0)
+    length(docker_container.app_container) > 0 ? (
+      "http://localhost:${docker_container.app_container[0].ports[0].external}"
+    ) : (
+      "Container not deployed or not running in this mode. Check 'var.mode'."
+    )
+  ) : (
+    "Container not configured to be deployed in 'image_only' mode."
+  )
 }
 
-output "docker_image_id" {
-  description = "ID of the built Docker image."
-  value       = length(docker_image.app_image) > 0 ? docker_image.app_image[0].image_id : "Docker image not built (mode not 'image_only' or 'full')."
-  sensitive   = false
+output "docker_image_name" {
+  description = "The name of the Docker image that was built or would be used."
+  value = var.mode == "image_only" || var.mode == "full" ? (
+    length(docker_image.app_image) > 0 ? docker_image.app_image[0].name : var.image_name # Fallback to var.image_name if image resource count is 0
+  ) : var.image_name
 }
