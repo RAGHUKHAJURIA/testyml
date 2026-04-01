b387a3f35f843130ba9f46d385cba9a9b4253fc3
Raghu <106529589+RAGHUKHAJURIA@users.noreply.github.com>
2026-04-02 03:28:55 +0530

    [NeuroOps] Auto-Setup: Initializing CI/CD structural changes natively

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-# Existing variables from the original file (if any) go here
-# ...
-
-variable "docker_image" {
-  description = "Docker image to deploy (e.g., ghcr.io/user/repo:tag)"
+variable "image_full_tag" {
+  description = "Fully qualified Docker image tag to deploy (e.g., ghcr.io/org/repo:sha)"
   type        = string
 }
+
+# Existing variables.tf content would be here, if any:
+# ...
