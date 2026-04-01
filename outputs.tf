36075e4bcdaa945c1bbfe2550f2bc46d8c2ad418
Raghu <106529589+RAGHUKHAJURIA@users.noreply.github.com>
2026-04-02 01:24:03 +0530

    [NeuroOps] IaC Setup: Auto-provisioning foundation infrastructure natively (Terraform)

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,19 @@
-output "staging_url" {
-  description = "The public URL of the staging environment."
-  value       = "https://staging.${var.domain_name}"
+output "container_id" {
+  description = "The ID of the deployed Docker container."
+  value       = docker_container.app_container.id
 }
 
-output "production_url" {
-  description = "The public URL of the production environment."
-  value       = "https://${var.domain_name}"
+output "container_name" {
+  description = "The name of the deployed Docker container."
+  value       = docker_container.app_container.name
 }
 
-output "staging_cloudfront_domain" {
-  description = "The domain name of the staging CloudFront distribution."
-  value       = aws_cloudfront_distribution.staging.domain_name
+output "container_ip_address" {
+  description = "The IP address of the deployed Docker container."
+  value       = docker_container.app_container.ip_address
 }
 
-output "production_cloudfront_domain" {
-  description = "The domain name of the production CloudFront distribution."
-  value       = aws_cloudfront_distribution.production.domain_name
-}
-
-output "s3_staging_bucket_name" {
-  description = "The name of the S3 bucket for staging content."
-  value       = aws_s3_bucket.staging.id
-}
-
-output "s3_production_bucket_name" {
-  description = "The name of the S3 bucket for production content."
-  value       = aws_s3_bucket.production.id
-}
-
-output "route53_zone_id" {
-  description = "The ID of the Route 53 Hosted Zone for the primary domain."
-  value       = aws_route53_zone.primary.zone_id
-}
-
-output "route53_name_servers" {
-  description = "The name servers for the Route 53 Hosted Zone. These must be configured with your domain registrar."
-  value       = aws_route53_zone.primary.name_servers
+output "host_access_url" {
+  description = "The URL to access the application on the host machine."
+  value       = "http://localhost:${docker_container.app_container.ports[0].external}"
 }
