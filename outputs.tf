61d8ef2b20a3f5330a9d5655f433a0045fb50183
Raghu <106529589+RAGHUKHAJURIA@users.noreply.github.com>
2026-04-02 03:11:18 +0530

    [NeuroOps] IaC Setup: Auto-provisioning foundation infrastructure natively (Terraform)

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,9 @@
 output "container_url" {
-  description = "The URL to access the running Docker container, if deployed."
-  value = var.mode == "container_only" || var.mode == "full" ? (
-    # Check if the container resource was actually created (count > 0)
-    length(docker_container.app_container) > 0 ? (
-      "http://localhost:${docker_container.app_container[0].ports[0].external}"
-    ) : (
-      "Container not deployed or not running in this mode. Check 'var.mode'."
-    )
-  ) : (
-    "Container not configured to be deployed in 'image_only' mode."
-  )
+  description = "The URL to access the running Docker container, if applicable."
+  value       = length(docker_container.app_container) > 0 ? "http://localhost:${docker_container.app_container[0].ports[0].external}" : "Container not running in this mode."
 }
 
-output "docker_image_name" {
-  description = "The name of the Docker image that was built or would be used."
-  value = var.mode == "image_only" || var.mode == "full" ? (
-    length(docker_image.app_image) > 0 ? docker_image.app_image[0].name : var.image_name # Fallback to var.image_name if image resource count is 0
-  ) : var.image_name
+output "image_name_built" {
+  description = "The full name of the Docker image that was built, if applicable."
+  value       = length(docker_image.app_image) > 0 ? docker_image.app_image[0].name : "Image not built in this mode."
 }
