39c66d079b5313c6358cf8bf727e656250549397
Raghu <106529589+RAGHUKHAJURIA@users.noreply.github.com>
2026-04-02 03:28:56 +0530

    [NeuroOps] Auto-Setup: Initializing CI/CD structural changes natively

--- main.tf
+++ main.tf
@@ -1,35 +1,33 @@
-### Update within your existing aws_ecs_task_definition resource ###
+# Update your existing main.tf to use the 'image_full_tag' variable for your container definition.
+# For example, if you are deploying to AWS ECS, you would update an 'aws_ecs_task_definition' resource.
 
-# Locate your `aws_ecs_task_definition` resource block.
-# Inside its `container_definitions` argument, update the 'image' field 
-# to use the `var.docker_image` variable.
+# Example modification (adjust to your specific resource, e.g., Kubernetes Deployment, other container service):
 
-# Example of the change needed:
-# resource "aws_ecs_task_definition" "app" {
-#   ...
-#   container_definitions = jsonencode([
-#     {
-#       name      = "my-app-container"
-#       image     = var.docker_image  # <-- Change this line
-#       cpu       = 256
-#       memory    = 512
-#       essential = true
-#       portMappings = [
-#         {
-#           containerPort = 8080
-#           hostPort      = 8080
-#           protocol      = "tcp"
-#         }
-#       ]
-#       logConfiguration = {
-#         logDriver = "awslogs"
-#         options = {
-#           "awslogs-group"         = "/ecs/my-dockerized-app"
-#           "awslogs-region"        = var.aws_region
-#           "awslogs-stream-prefix" = "ecs"
-#         }
-#       }
-#     }
-#   ])
-#   ...
-# }
+# Find your container definition resource, e.g.:
+resource "aws_ecs_task_definition" "my_app_task" {
+  # ... existing attributes ...
+
+  # Update the 'image' field within your container_definitions JSON:
+  container_definitions = jsonencode([
+    {
+      name  = "my-dockerized-app",
+      image = var.image_full_tag, # <-- Use the variable here
+      cpu   = 256,
+      memory= 512,
+      essential = true,
+      portMappings = [
+        {
+          containerPort = 8080, # Matches Dockerfile EXPOSE
+          hostPort      = 8080,
+          protocol      = "tcp"
+        }
+      ],
+      # ... other container configurations from your existing setup ...
+    }
+  ])
+
+  # ... other task definition settings ...
+}
+
+# Existing main.tf content would be here:
+# ...
