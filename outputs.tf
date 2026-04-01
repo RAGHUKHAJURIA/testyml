edcad1f090f4c70da7870d20f684fb65d80b19c8
Raghu <106529589+RAGHUKHAJURIA@users.noreply.github.com>
2026-04-02 01:54:39 +0530

    [NeuroOps] IaC Setup: Auto-provisioning foundation infrastructure natively (Terraform)

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,9 @@
-output "container_id" {
-  description = "The ID of the running Docker container."
-  value       = docker_container.app_container.id
+output "container_url" {
+  description = "URL to access the running Docker container locally."
+  value       = "http://localhost:${docker_container.static_web_container.ports[0].external}"
 }
 
-output "container_name" {
-  description = "The name of the running Docker container."
-  value       = docker_container.app_container.name
-}
-
-output "container_ip_address" {
-  description = "The IP address of the Docker container within the Docker network."
-  value       = docker_container.app_container.ip_address
-}
-
-output "host_url" {
-  description = "The URL to access the application on the host machine."
-  value       = "http://localhost:${docker_container.app_container.ports[0].external}"
+output "image_id" {
+  description = "The ID of the built Docker image."
+  value       = docker_image.static_web_app.id
 }
