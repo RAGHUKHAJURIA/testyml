db98c4e8fa5c81472a3d33e26d75554df560ab6a
Raghu <106529589+RAGHUKHAJURIA@users.noreply.github.com>
2026-04-02 01:50:46 +0530

    [NeuroOps] IaC Setup: Auto-provisioning foundation infrastructure natively (Terraform)

--- variables.tf
+++ variables.tf
@@ -1,29 +1,35 @@
-variable "app_name" {
-  description = "The name of the application."
+variable "image_name" {
+  description = "Name and tag for the Docker image to be built."
   type        = string
-  default     = "my-app"
+  default     = "my-static-web-app:latest"
 }
 
-variable "image_tag" {
-  description = "Tag for the Docker image."
+variable "container_name" {
+  description = "Name for the Docker container."
   type        = string
-  default     = "latest"
+  default     = "static-web-app-container"
 }
 
 variable "container_port" {
-  description = "The internal port the application listens on inside the Docker container."
+  description = "The port exposed by the application inside the Docker container (as per Dockerfile EXPOSE)."
   type        = number
   default     = 8080
 }
 
-variable "staging_host_port" {
-  description = "The host port to map to the staging container's internal port."
+variable "host_port" {
+  description = "The port on the host machine to map to the container's exposed port."
   type        = number
-  default     = 8081 # Common practice to use different ports for local environments
+  default     = 8080 # Default to the same as container port, can be overridden
 }
 
-variable "production_host_port" {
-  description = "The host port to map to the production container's internal port."
-  type        = number
-  default     = 8080 # Standard HTTP port locally
+variable "dockerfile_path" {
+  description = "Path to the Dockerfile relative to the build context."
+  type        = string
+  default     = "Dockerfile"
+}
+
+variable "build_context_path" {
+  description = "Path to the Docker image build context (where Dockerfile and 'dist' directory reside), relative to the Terraform root module."
+  type        = string
+  default     = "."
 }
