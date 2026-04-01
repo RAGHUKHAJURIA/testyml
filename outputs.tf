5e01283cf19cdf80d945fbf9cdc7ee4744446ae2
Raghu <106529589+RAGHUKHAJURIA@users.noreply.github.com>
2026-04-02 02:24:19 +0530

    [NeuroOps] IaC Setup: Auto-provisioning foundation infrastructure natively (Terraform)

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,11 @@
 output "container_url" {
-  description = "The URL to access the deployed Docker container."
-  value       = try(
-    "http://localhost:${docker_container.app_container[0].ports[0].external}",
-    "Container not deployed in this mode (mode: ${var.mode})"
-  )
-  # The sensitive flag can be used if the URL contains sensitive information, though unlikely for localhost.
-  # sensitive = true
+  description = "URL to access the deployed Docker container."
+  value       = length(docker_container.app_container) > 0 ? "http://localhost:${docker_container.app_container[0].ports[0].external}" : "Container not deployed (mode not 'container_only' or 'full')."
+  sensitive   = false
 }
 
-output "image_name" {
-  description = "The full name of the Docker image that was built or would be used."
-  value = var.mode == "container_only" ? var.pre_built_image_full_name : try(
-    docker_image.app_image[0].name,
-    "Image not built in this mode (mode: ${var.mode})"
-  )
+output "docker_image_id" {
+  description = "ID of the built Docker image."
+  value       = length(docker_image.app_image) > 0 ? docker_image.app_image[0].image_id : "Docker image not built (mode not 'image_only' or 'full')."
+  sensitive   = false
 }
